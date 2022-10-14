f74f93a5c8dde304dbd117f889b8632b22e3d6cf
Nfaúndez <nat.faundezc@gmail.com>
2022-10-14 14:00:25 -0300

    aprobados los test, funciona cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,54 @@
 const cipher = {
-  // ...
-};
-
+    encode: function(offset, message) {
+        if (message === undefined || offset === undefined) {
+            throw new TypeError("alertaaa!!!")
+        } else if (message === typeof 0 || offset === 0) {
+            throw new TypeError("alertaaa!!!")
+        }
+        const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        offset = (offset % 26 + 26) % 26;
+        return message.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) + offset) % 26]);
+    },
+        
+    decode: function(offset, message) {
+        if (message === undefined || offset === undefined) {
+            throw new TypeError("alertaaa!!!")
+        } else if (message === typeof 0 || offset === 0) {
+            throw new TypeError("alertaaa!!!")
+        }
+        const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        offset = (offset % 26 - 26) % 26;
+        return message.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) - offset) % 26]);
+    }
+}
 export default cipher;
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import cipher from './cipher.js';
+let offset = document.getElementById("desplazamiento");
+const message = document.getElementById('messageencode');
+document.getElementById('encode').addEventListener('click', ()=>{
+    const encodedmessage= cipher.encode(offset.value, message.value);
+    console.log (encodedmessage)
+    document.getElementById("messagedecode").value = encodedmessage;
+}, )
+document.getElementById("decode").addEventListener("click", function(){
+   
+    document.getElementById("messagedecode").value = cipher.decode( offset.value, message.value);
+}, )
+    
+    
+   
+// aqui se dio funcion al boton codificar con click
+    // debo llamar aqui al boton del offset
 
-document.getElementById('code').addEventListener('click', ()=>{
-    const message = document.getElementById('message').value;
-    console.log(message)
-})
-console.log(cipher);
+    
+
+  // aqui se dio funcion al boton decodificar con click
+    // debo arreglar que esto pase en messagecode
+    
